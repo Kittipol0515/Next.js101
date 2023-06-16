1f13fd69880bd85650f09e064f28ba89400f7d15
kittipol <kittipol.surapinchai@gmail.com>
2023-06-16 23:31:56 +0700

    Next 101

---
+++ components/Navbar.js
@@ -0,0 +1,14 @@
+export default function Navbar() {
+    return(
+        <nav>
+        <div>
+            <h1>Nomain</h1>
+        </div>
+            <ul>
+                <li>Home page</li>
+                <li>About us</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    )
+}
