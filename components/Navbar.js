595ff16f5db1d02e1368be3e8ec302feba70a73d
kittipol <kittipol.surapinchai@gmail.com>
2023-06-17 15:21:32 +0700

    Next101

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,14 +1,13 @@
+import Link from "next/link"
 export default function Navbar() {
     return(
         <nav>
-        <div>
-            <h1>Nomain</h1>
-        </div>
-            <ul>
-                <li>Home page</li>
-                <li>About us</li>
-                <li>Contact</li>
-            </ul>
+            <div>
+                <h1>Shopping</h1>
+            </div>
+            <Link href="/">Home page</Link>
+            <Link href="/about">about</Link>
+            <Link href="/products">all products</Link>
         </nav>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -2,17 +2,14 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-import Navbar from '@/components/Navbar'
-import Footer from '@/components/Footer'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <>
-    <Navbar />
      <h1>Home page</h1>
-    <Footer />
     </>
   )
 }
